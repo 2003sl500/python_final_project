1167e0d98fdefb9d08817c47ee0efcd5568b913b
Daniel Shoemaker <danielshoe@charter.net>
2021-08-26 14:24:59 -0700

    added and updated html

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,32 +1,96 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.client import Clients
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def base_route():
     if 'cuid' in session:
         session.clear()
         return redirect('/')
+    session['form'] = [{}]
     return render_template('index.html')
-
-@app.route('/register')
-def register():
-    if 'cuid' in session:
-        session.clear()
-        return redirect('/')
-    return render_template('clients.html')
 
 @app.route('/login')
 def login():
     if 'cuid' in session:
         session.clear()
-        return redirect('/')
+        return redirect('/')    
     return render_template('clients.html')
 
-@app.route('/clients', methods=['POST'])
-def clients():
+@app.route('/login/user', methods=['POST'])
+def login_user():
+    if 'cuid' in session:
+        del session['cuid']
+        return redirect('/')
+        
+    user_info = Clients.find_email(request.form['email_login'])
+    print('********* user_info: ', user_info)
+    if not user_info:
+        session['email_login'] = request.form['email_login']
+        flash("Invalid Email/Password", "login")
+        return redirect('/login')
     
-    return redirect('/')
+    if not Clients.validate_login(request.form):    
+        session['email_login'] = request.form['email_login']
+        return redirect('/login')
+    
+    if not bcrypt.check_password_hash(user_info[0]['password'], request.form['password_login']):
+        flash("Invalid Email/Password", "login")
+        return redirect('/login')
+    
+    session['cuid'] = user_info[0]['id']
+    
+    session['fname'] = user_info[0]['fname']
+    
+    return redirect('/user')
+
+@app.route('/registration')
+def registration():
+    if 'cuid' in session:
+        session.clear()
+        return redirect('/')
+    
+    
+    return render_template('clients.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if 'cuid' in session:
+        session.clear()
+        return redirect('/')
+    
+    if not Clients.validate_reg(request.form):
+        session['form'] = request.form
+        return redirect('/login')
+    
+    session.clear()
+    session['fname'] = request.form['fname']
+    
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email'],
+        'address': request.form['address'],
+        'city': request.form['city'],
+        'state': request.form['state'],
+        'zip': request.form['zip'], 
+        'password': pw_hash
+    }
+    
+    cuid = Clients.create(data)
+    session['cuid'] = cuid
+    
+    return redirect('/user')
+
+@app.route('/user')
+def user():
+    if 'cuid' not in session:  
+        return redirect('/login')
+    return render_template('user.html')
 
 @app.route('/logout')
 def logout():
@@ -65,5 +129,11 @@ def keyboards_combo():
 
 @app.route('/storage')
 def storage():
-    
+    session['temp'] = [{'name': 'daniel', 'age': '48'}]
+    print('********** temp:', session['temp'][0]['name'])
     return render_template('storage.html')
+
+@app.route('/product_input')
+def product_input():
+    
+    return render_template('product_input.html')
--- flask_app/models/client.py
+++ flask_app/models/client.py
@@ -1,4 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
 
 
 DB = 'python_proj'
@@ -9,9 +11,76 @@ class Clients:
         self.id = data['id']
         self.fname = data['fname']
         self.lname = data['lname']
+        self.email = data['email']
         self.address = data['address']
         self.city = data['city']
         self.state = data['state']
         self.zip = data['zip']
+        self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        
+    @classmethod
+    def get_all(cls):
+        query = f'SELECT * FROM {CLIENTS};'
+        results = connectToMySQL(DB).query_db(query)
+        information = []
+        for info in results:
+            information.append( cls(info) )
+        return information
+    
+    @classmethod
+    def create(cls, data):
+        query = f'INSERT INTO {CLIENTS}( fname, lname, email, address, city, state, zip, password, updated_at ) VALUES( %(fname)s, %(lname)s, %(email)s, %(address)s, %(city)s, %(state)s, %(zip)s, %(password)s, NOW() );'
+        return connectToMySQL(DB).query_db(query, data)
+    
+    @classmethod
+    def find_email(cls, email):
+        query = f'SELECT id, fname, lname, password FROM {CLIENTS} WHERE email = %(email)s;'
+        email = {
+            'email': email
+        }
+        return connectToMySQL(DB).query_db(query, email)
+    
+    @staticmethod
+    def validate_login(form_data):
+        is_valid = True
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_i]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if (form_data['email_login'] == "") or (form_data['password_login'] == ""):
+            flash("Email and Password are required", "login")
+            is_valid = False
+            return is_valid
+        return is_valid
+    
+    @staticmethod
+    def validate_reg(form_data):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_i]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        is_valid = True
+        if (form_data['fname'] == ""):
+            flash("First Name is required", "fname")
+            is_valid = False
+        elif (len(form_data['fname']) < 3):
+            flash("First name must be at least 3 characters", "fname")
+            is_valid = False
+        if (form_data['lname'] == ""):
+            flash("Last Name is required", "lname")
+        elif (len(form_data['lname']) < 3):
+            flash("Last name must be at least 3 characters", "lname")
+            is_valid = False
+        if (form_data['email'] == ""):
+            flash("Email is required", "email")
+            is_valid = False
+        if Clients.find_email(form_data['email']):
+            flash("You are already registered", "email")
+            is_valid = False
+            return is_valid
+        elif not (EMAIL_REGEX.match(form_data['email'])):
+            flash("Email is not valid", "email")
+            is_valid = False
+        if len(form_data['password']) < 8:
+            flash("Password must be at least 8 characters long", "password")
+            is_valid = False
+        elif (form_data['password']) != (form_data['conf_password']):
+            flash("Password does not match Confirm Password", "conf_password")
+            is_valid = False
+        return is_valid
